5242a932d4de71e3cd851f1fbdaf75fff908c7dc
baric6 <baric0748@gmail.com>
2021-07-07 22:36:42 -0400

    added check function for first and last block

--- block-chain.py
+++ block-chain.py
@@ -18,22 +18,52 @@ if "__main__" == __name__:
             if blockChain[blockIndex].previousBlock == "None":
                 print("this is the first block")
                 print(f"next Block ID {blockChain[blockIndex].nextBlock}")
+
+                #checking right to left blocks
+                if blockChain[blockIndex].shaID == blockChain[blockIndex+1].previousBlock:
+                    print(f"chain is unbroken between {blockIndex} and {blockIndex+1} \n")
+                    print(f"this block id:                 {blockChain[blockIndex].shaID}")
+                    print(f"next block right pointer:      {blockChain[blockIndex+1].previousBlock}")
+                else:
+                    print(f"this block id:                {blockChain[blockIndex].shaID}")
+                    print(f"next block right pointer:      {blockChain[blockIndex+1].previousBlock}")
+                    print("chain is broken")  
+                    print("======================================================\n")
+
+                #left to right
+                if hash(blockChain[blockIndex]) == blockChain[blockIndex].nextBlock:
+                    print(f"chain is unbroken between {blockIndex+1} and {blockIndex} \n")
+                    print(f"next block id:                {hash(blockChain[blockIndex])}")
+                    print(f"this block left pointer:      {blockChain[blockIndex].nextBlock}\n")
+                else:
+                    print(f"next block id:                {hash(blockChain[blockIndex])}")
+                    print(f"this block left pointer:      {blockChain[blockIndex].nextBlock}")
+                    print("chain is broken")  
+                    print("======================================================\n")    
+
+
             elif blockChain[blockIndex].nextBlock == "None":
                 ######################################################################################
                 #check last block
                 print("this is the last block")
                 print(f"previous Block ID {blockChain[blockIndex].previousBlock}")
 
+                #this checks the left side of the last block
                 if hash(blockChain[blockIndex-2]) == blockChain[blockIndex].previousBlock:
-                    print(f"chain is unbroken between {blockIndex -1} and {blockIndex} \n")
+                    print(f"chain is unbroken between {blockIndex -1} and {blockIndex}")
+                    print(f"previous block id:             {hash(blockChain[blockIndex-2])}")
+                    print(f"this block left block pointer: {blockChain[blockIndex].previousBlock}")
                 else:
                     print("chain is broken")  
 
                 #this was a stupid bug so id i put [blockIndex-2].nextBlock it would get the previous id of the
                 #last block i had to make the index one less then the searched index for it to work
                 nb = blockIndex -1
+                #this checks the right side of the second to last block
                 if hash(blockChain[blockIndex-1]) == blockChain[nb].nextBlock:
                     print(f"chain is unbroken between {blockIndex -1} and {blockIndex} \n")
+                    print(f"this block id:                {hash(blockChain[blockIndex-1])}")
+                    print(f"previous block right pointer: {blockChain[nb].nextBlock:}")
                 else:
                     print("chain is broken")  
                     print("======================================================\n")
@@ -60,7 +90,7 @@ if "__main__" == __name__:
 
     print(len(blockChain))
 
-    #checkBlocks(0)
+    checkBlocks(0)
     #checkBlocks(1)
     checkBlocks(3)
     #checkBlocks(7)
